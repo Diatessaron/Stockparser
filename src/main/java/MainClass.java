52d639bdc16cf0d5523ee68bfacd70bf422878db
Azat Fazlyev <fazliev14@gmail.com>
2020-10-07 16:32:13 +0300

    Latest fixes

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -10,10 +10,11 @@ public class MainClass {
     public static void main(String[] args) {
         View view = new ConsoleView();
         Model model = new Model();
-//        Strategy strategy = new NASDAQStrategy();
-        Strategy strategy = new DowJonesStrategy();
+        Strategy strategy = new NASDAQStrategy();
+//        Strategy strategy = new DowJonesStrategy();
         Controller controller = new Controller(view, model, strategy);
 
+        controller.getStockByString("Apple");
         int i=0;
         do {
             controller.getAllStocks();
